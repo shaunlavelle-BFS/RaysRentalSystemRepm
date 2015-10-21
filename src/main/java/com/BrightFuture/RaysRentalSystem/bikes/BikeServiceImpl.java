30c2e7d9ffaa2ec8cdf80fb30ba2c0df839f38a5
slavelle <slavelle@mfb00309_slave.myfuturesbright.local>
2015-10-21 09:39:36 +0100

    Reverted changes resolved error

--- src/main/java/com/BrightFuture/RaysRentalSystem/bikes/BikeServiceImpl.java
+++ src/main/java/com/BrightFuture/RaysRentalSystem/bikes/BikeServiceImpl.java
@@ -2,25 +2,15 @@ package com.BrightFuture.RaysRentalSystem.bikes;
 
 import javax.transaction.Transactional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.BrightFuture.lib.dao.CRUDDAO;
-import com.BrightFuture.lib.service.CRUDService;
 
 @Service
 @Transactional
 public class BikeServiceImpl implements BikeService {
 
-	@Autowired
-	private CRUDService crudService;
-	
-	@Autowired
-	private CRUDDAO cruddao;
-	
 	@Override
 	public Bike addNewBike(Bike bike) {
-		
+		// TODO
 		return null;
 	}
 
